e4a5b0b751dcca7fe3307b9abf727a004a25ccf4
Renaud Lepage <root@cybikbase.com>
2014-03-10 21:22:52 -0400

    Correct Lua Android makefile.

--- extern/lua-5.1/Android.mk
+++ extern/lua-5.1/Android.mk
@@ -4,37 +4,38 @@ LOCAL_PATH := $(LOC_LUA_PATH)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE:= lua
-LOCAL_SRC_FILES := src/lauxlib.c \
-		  src/lmathlib.c \
-		  src/loslib.c \
-		  src/ltable.c \
-		  src/print.c \
+LOCAL_SRC_FILES := src/lapi.c \
+	          src/lauxlib.c \
+		  src/lbaselib.c \
+		  src/lcode.c \
+		  src/ldblib.c \
+		  src/ldebug.c \
+		  src/ldo.c \
+		  src/ldump.c \
+		  src/lfunc.c \
+		  src/lgc.c \
+		  src/linit.c \
+		  src/liolib.c \
 		  src/llex.c \
-		  src/lvm.c \
-		  src/lapi.c \
+		  src/lmathlib.c \
+		  src/lmem.c \
+		  src/loadlib.c \
 		  src/lobject.c \
+		  src/lopcodes.c \
+		  src/loslib.c \
+		  src/lparser.c \
+		  src/lstate.c \
+		  src/lstring.c \
+		  src/lstrlib.c \
+		  src/ltable.c \
+		  src/ltablib.c \
 		  src/ltm.c \
 		  src/lundump.c \
-		  src/lopcodes.c \
-		  src/luac.c \
-		  src/lbaselib.c \
-		  src/lgc.c \
-		  src/ltablib.c \
-		  src/loadlib.c \
-		  src/ldump.c \
-		  src/lcode.c \
-		  src/linit.c \
+		  src/lvm.c \
 		  src/lzio.c \
-		  src/lstate.c \
-		  src/lparser.c \
-		  src/ldo.c \
-		  src/lstring.c \
-		  src/lmem.c \
-		  src/ldblib.c \
-		  src/lstrlib.c \
-		  src/liolib.c \
-		  src/lfunc.c \
-		  src/ldebug.c
+		  src/print.c
+
+#		  src/luac.c \
 #		  src/lua.c \
 		  
 LOCAL_C_INCLUDES := $(LOC_LUA_PATH)/src
